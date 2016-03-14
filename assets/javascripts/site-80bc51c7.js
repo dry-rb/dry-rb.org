3c6ed957cc07d350244155c48487795cb073fcb4
Piotr Solnica <piotr.solnica@gmail.com>
2016-03-14 13:55:06 +0100

    Automated commit at 2016-03-14 12:55:06 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/site-80bc51c7.js
+++
@@ -1,1 +0,0 @@
-!function(o){function n(t){if(r[t])return r[t].exports;var e=r[t]={exports:{},id:t,loaded:!1};return o[t].call(e.exports,e,e.exports,n),e.loaded=!0,e.exports}var r={};return n.m=o,n.c=r,n.p="",n(0)}([function(o,n,r){r(1),o.exports=r(2)},function(o,n){domready(function(){console.log("domready")})},function(o,n){}]);
