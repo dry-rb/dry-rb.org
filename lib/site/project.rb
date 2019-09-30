92e967878336c26b3b4796701de311519fe1fd32
Piotr Solnica <piotr.solnica@gmail.com>
2019-09-30 11:11:48 +0200

    Dry-up Project struct

--- lib/site/project.rb
+++ lib/site/project.rb
@@ -10,8 +10,6 @@ module Site
     attribute?(:current_version, Types::String)
     attribute?(:fallback_version, Types::String)
 
-    alias_method :to_s, :name
-
     # Convert this config:
     #
     # versions:
@@ -32,42 +30,6 @@ module Site
       end
     end
 
-    def github_url
-      "https://github.com/dry-rb/#{name}"
-    end
-
-    def rubygems_url
-      "https://rubygems.org/gems/#{name}"
-    end
-
-    def version_badge
-      "https://img.shields.io/gem/v/#{name}.svg?style=flat"
-    end
-
-    def ci_badge
-      "https://img.shields.io/travis/dry-rb/#{name}/master.svg?style=flat"
-    end
-
-    def codeclimate_url
-      "https://codeclimate.com/github/dry-rb/#{name}"
-    end
-
-    def codeclimate_badge
-      "https://codeclimate.com/github/dry-rb/#{name}/badges/gpa.svg"
-    end
-
-    def coverage_badge
-      "https://codeclimate.com/github/dry-rb/#{name}/badges/coverage.svg"
-    end
-
-    def inch_url
-      "http://inch-ci.org/github/dry-rb/#{name}"
-    end
-
-    def inch_badge
-      "http://inch-ci.org/github/dry-rb/#{name}.svg?branch=master&style=flat"
-    end
-
     def opened_issues_badge
       "https://img.shields.io/github/issues/dry-rb/#{name}.svg?branch=master&style=flat"
     end
@@ -82,6 +44,10 @@ module Site
 
     def opened_pulls_url
       "#{github_url}/pulls"
+    end
+
+    def org
+      'dry-rb'
     end
   end
 end
